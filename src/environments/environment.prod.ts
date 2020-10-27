18909a3b54ec3ddea627b310019cb49395913469
Sathit Seethaphon <dixonsatit@gmail.com>
2020-10-27 15:11:23 +0700

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:3000',
+  apiUrl: 'http://api:3000',
   apiUrlLogin: 'http://iconnect.kkh.go.th:3008/login/old-user'
 };
